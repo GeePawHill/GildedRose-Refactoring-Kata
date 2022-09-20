8b6d5d29b45d272f00e9fda278febfdb169e8059
GeePaw Hill <geepawhill@geepawhill.org>
2022-09-20 16:51:27 -0400

    First test, normal sulfuras never changes.

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,17 +1,19 @@
 package com.gildedrose
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class GildedRoseTest {
+class GildedRoseTest {
 
     @Test
-    fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+    fun `sulfuras never changes`() {
+        val items = arrayOf(Item(SULFURUS_NAME, Int.MAX_VALUE, 80))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
-
+        assertThat(items[0].name).isEqualTo(SULFURUS_NAME)
+        assertThat(items[0].sellIn).isEqualTo(Int.MAX_VALUE)
+        assertThat(items[0].quality).isEqualTo(80)
     }
 
 }
