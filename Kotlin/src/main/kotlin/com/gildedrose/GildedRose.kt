2c9b18aa8d5fa64a66f41ac560fe7f236f0ed0de
GeePaw Hill <geepawhill@geepawhill.org>
2022-09-20 18:19:03 -0400

    GenericUpdater added.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -40,11 +40,23 @@ class BackstageUpdater {
     }
 }
 
+class GenericUpdater {
+    fun updateQuality(item: Item) {
+        with(item) {
+            sellIn -= 1
+            quality -= 1
+            if (sellIn < 0) quality -= 1
+            if (quality < 0) quality = 0
+        }
+    }
+}
+
 class GildedRose(var items: Array<Item>) {
 
     private val sulfurusUpdater = SulfurusUpdater()
     private val brieUpdater = BrieUpdater()
     private val backstageUpdater = BackstageUpdater()
+    private val genericUpdater = GenericUpdater()
 
     fun updateQuality() {
         for (item in items) {
@@ -54,64 +66,11 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality(item: Item) {
         with(item) {
-            if (name == SULFURUS_NAME) {
-                sulfurusUpdater.updateQuality(item)
-                return
-            }
-            if (name == BRIE_NAME) {
-                brieUpdater.updateQuality(item)
-                return
-            }
-            if (name == BACKSTAGE_NAME) {
-                backstageUpdater.updateQuality(item)
-                return
-            }
-            if (name != BRIE_NAME && name != BACKSTAGE_NAME) {
-                if (quality > 0) {
-                    if (name != SULFURUS_NAME) {
-                        quality = quality - 1
-                    }
-                }
-            } else {
-                if (quality < MAX_NORMAL_QUALITY) {
-                    quality = quality + 1
-
-                    if (name == BACKSTAGE_NAME) {
-                        if (sellIn < BACKSTAGE_FIRST_UPGRADE) {
-                            if (quality < MAX_NORMAL_QUALITY) {
-                                quality = quality + 1
-                            }
-                        }
-
-                        if (sellIn < BACKSTAGE_SECOND_UPGRADE) {
-                            if (quality < MAX_NORMAL_QUALITY) {
-                                quality = quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (name != SULFURUS_NAME) {
-                sellIn = sellIn - 1
-            }
-
-            if (sellIn < 0) {
-                if (name != BRIE_NAME) {
-                    if (name != BACKSTAGE_NAME) {
-                        if (quality > 0) {
-                            if (name != SULFURUS_NAME) {
-                                quality = quality - 1
-                            }
-                        }
-                    } else {
-                        quality = quality - quality
-                    }
-                } else {
-                    if (quality < MAX_NORMAL_QUALITY) {
-                        quality = quality + 1
-                    }
-                }
+            when (name) {
+                SULFURUS_NAME -> sulfurusUpdater.updateQuality(item)
+                BRIE_NAME -> brieUpdater.updateQuality(item)
+                BACKSTAGE_NAME -> backstageUpdater.updateQuality(item)
+                else -> genericUpdater.updateQuality(item)
             }
         }
     }
