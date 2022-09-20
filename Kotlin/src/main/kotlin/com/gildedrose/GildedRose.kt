c1b55c0ea94c44c5594110ff0415f4b8c3f3770e
GeePaw Hill <geepawhill@geepawhill.org>
2022-09-20 16:48:01 -0400

    Extracted loop body.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -13,51 +13,55 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            if (item.name != BRIE_NAME && item.name != BACKSTAGE_NAME) {
-                if (item.quality > 0) {
-                    if (item.name != SULFURUS_NAME) {
-                        item.quality = item.quality - 1
+            updateQuality(item)
+        }
+    }
+
+    fun updateQuality(item: Item) {
+        if (item.name != BRIE_NAME && item.name != BACKSTAGE_NAME) {
+            if (item.quality > 0) {
+                if (item.name != SULFURUS_NAME) {
+                    item.quality = item.quality - 1
+                }
+            }
+        } else {
+            if (item.quality < MAX_NORMAL_QUALITY) {
+                item.quality = item.quality + 1
+
+                if (item.name == BACKSTAGE_NAME) {
+                    if (item.sellIn < BACKSTAGE_FIRST_UPGRADE) {
+                        if (item.quality < MAX_NORMAL_QUALITY) {
+                            item.quality = item.quality + 1
+                        }
                     }
+
+                    if (item.sellIn < BACKSTAGE_SECOND_UPGRADE) {
+                        if (item.quality < MAX_NORMAL_QUALITY) {
+                            item.quality = item.quality + 1
+                        }
+                    }
+                }
+            }
+        }
+
+        if (item.name != SULFURUS_NAME) {
+            item.sellIn = item.sellIn - 1
+        }
+
+        if (item.sellIn < 0) {
+            if (item.name != BRIE_NAME) {
+                if (item.name != BACKSTAGE_NAME) {
+                    if (item.quality > 0) {
+                        if (item.name != SULFURUS_NAME) {
+                            item.quality = item.quality - 1
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality
                 }
             } else {
                 if (item.quality < MAX_NORMAL_QUALITY) {
                     item.quality = item.quality + 1
-
-                    if (item.name == BACKSTAGE_NAME) {
-                        if (item.sellIn < BACKSTAGE_FIRST_UPGRADE) {
-                            if (item.quality < MAX_NORMAL_QUALITY) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-
-                        if (item.sellIn < BACKSTAGE_SECOND_UPGRADE) {
-                            if (item.quality < MAX_NORMAL_QUALITY) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.name != SULFURUS_NAME) {
-                item.sellIn = item.sellIn - 1
-            }
-
-            if (item.sellIn < 0) {
-                if (item.name != BRIE_NAME) {
-                    if (item.name != BACKSTAGE_NAME) {
-                        if (item.quality > 0) {
-                            if (item.name != SULFURUS_NAME) {
-                                item.quality = item.quality - 1
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if (item.quality < MAX_NORMAL_QUALITY) {
-                        item.quality = item.quality + 1
-                    }
                 }
             }
         }
