a37f9ef195b2c7cb7e3bc55a02d30d737234939e
GeePaw Hill <geepawhill@geepawhill.org>
2022-09-20 16:59:06 -0400

    Made individual updateQuality use with and killed some dots off.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -18,54 +18,55 @@ class GildedRose(var items: Array<Item>) {
     }
 
     fun updateQuality(item: Item) {
-        if (item.name != BRIE_NAME && item.name != BACKSTAGE_NAME) {
-            if (item.quality > 0) {
-                if (item.name != SULFURUS_NAME) {
-                    item.quality = item.quality - 1
-                }
-            }
-        } else {
-            if (item.quality < MAX_NORMAL_QUALITY) {
-                item.quality = item.quality + 1
-
-                if (item.name == BACKSTAGE_NAME) {
-                    if (item.sellIn < BACKSTAGE_FIRST_UPGRADE) {
-                        if (item.quality < MAX_NORMAL_QUALITY) {
-                            item.quality = item.quality + 1
-                        }
+        with(item) {
+            if (name != BRIE_NAME && name != BACKSTAGE_NAME) {
+                if (quality > 0) {
+                    if (name != SULFURUS_NAME) {
+                        quality = quality - 1
                     }
-
-                    if (item.sellIn < BACKSTAGE_SECOND_UPGRADE) {
-                        if (item.quality < MAX_NORMAL_QUALITY) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-                }
-            }
-        }
-
-        if (item.name != SULFURUS_NAME) {
-            item.sellIn = item.sellIn - 1
-        }
-
-        if (item.sellIn < 0) {
-            if (item.name != BRIE_NAME) {
-                if (item.name != BACKSTAGE_NAME) {
-                    if (item.quality > 0) {
-                        if (item.name != SULFURUS_NAME) {
-                            item.quality = item.quality - 1
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality
                 }
             } else {
-                if (item.quality < MAX_NORMAL_QUALITY) {
-                    item.quality = item.quality + 1
+                if (quality < MAX_NORMAL_QUALITY) {
+                    quality = quality + 1
+
+                    if (name == BACKSTAGE_NAME) {
+                        if (sellIn < BACKSTAGE_FIRST_UPGRADE) {
+                            if (quality < MAX_NORMAL_QUALITY) {
+                                quality = quality + 1
+                            }
+                        }
+
+                        if (sellIn < BACKSTAGE_SECOND_UPGRADE) {
+                            if (quality < MAX_NORMAL_QUALITY) {
+                                quality = quality + 1
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (name != SULFURUS_NAME) {
+                sellIn = sellIn - 1
+            }
+
+            if (sellIn < 0) {
+                if (name != BRIE_NAME) {
+                    if (name != BACKSTAGE_NAME) {
+                        if (quality > 0) {
+                            if (name != SULFURUS_NAME) {
+                                quality = quality - 1
+                            }
+                        }
+                    } else {
+                        quality = quality - quality
+                    }
+                } else {
+                    if (quality < MAX_NORMAL_QUALITY) {
+                        quality = quality + 1
+                    }
                 }
             }
         }
     }
-
 }
 
